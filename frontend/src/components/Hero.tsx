e83f86b1bb06eaac6f51e1a17390801e7ec99053
sagarkhadka <khadkasagar109@gmail.com>
2022-08-23 17:50:42 +0545

    hero section

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
+import style from '../styles/scss/Hero.module.scss'
 
 const Hero = () => {
   return (
-    <div className='container'>
-      
+    <div className={`${style.wrapper}`}>
+        <div className="container">
+            <div className={`${style.leftBody}`}>
+                <h1>Amazing Mountan to explore</h1>
+                <p>Do not follow where the path may lead. Go instead where there is no path and leave a trail.</p>
+            </div>
+        </div>
     </div>
   )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import CardCarousel from "../components/CardCarousel";
+import Hero from "../components/Hero";
 import HeroCarousel from "../components/HeroCarousel";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div className="home-page" style={{ backgroundColor: "red" }}>
-      <Navbar />
-      <HeroCarousel/>
+    <div className="home-page">
+      {/* <Navbar /> */}
+      {/* <HeroCarousel/> */}
+      <Hero />
     </div>
   );
 };
