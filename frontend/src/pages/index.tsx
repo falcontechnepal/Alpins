912f1c1523c18b1985b25b67ddbc61afb319240c
lokendra <lokendrachaulagain803@gmail.com>
2022-08-24 13:45:45 +0545

    loki

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import AboutSection from "../components/AboutSection";
 import CardCarousel from "../components/CardCarousel";
+import Footer from "../components/Footer";
 import HeroCarousel from "../components/HeroCarousel";
 import Navbar from "../components/Navbar";
+import NewsLetter from "../components/NewsLetter";
+
+import TopService from "../components/TopService";
 
 const Home = () => {
   return (
-    <div className="home-page" style={{ backgroundColor: "red" }}>
+    <div className="home-page">
       <Navbar />
-      <HeroCarousel />
+      {/* <HeroCarousel /> */}
       {/* <CardCarousel /> */}
+      <AboutSection />
+      <TopService />
+      <NewsLetter />
+      <Footer />
+      <CardCarousel />
     </div>
   );
 };
