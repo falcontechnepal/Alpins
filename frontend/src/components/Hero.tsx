07f7163213855456166b9d6f6a5f12ab16873ed0
sagarkhadka <khadkasagar109@gmail.com>
2022-08-24 10:37:12 +0545

    come changes in hero.tsx and module.scss

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,23 +5,25 @@ const Hero = () => {
   return (
     <div className={`${style.wrapper}`}>
         <div className={`${style.heroBody} container`}>
-            <div className={`${style.leftBody}`}>
-                <h1>Amazing Mountan to explore</h1>
-                <p>Do not follow where the path may lead. Go instead where there is no path and leave a trail.</p>
-            </div>
-            <div className={`${style.rightBody}`}>
-              <div className={style.stat}>
-                <span>Altitude</span>
-                <span>8848m</span>
-              </div>
-              <div className={style.stat}>
-                <span>Tracks</span>
-                <span>20</span>
-              </div>
-              <div className={style.stat}>
-                <span>Tourists/year</span>
-                <span>40000+</span>
-              </div>
+            <div className="row">
+                <div className={`${style.leftBody} col-md-6`}>
+                    <h1>Amazing Mountan to explore</h1>
+                    <p>Do not follow where the path may lead. Go instead where there is no path and leave a trail.</p>
+                </div>
+                <div className={`${style.rightBody} col-md-4`}>
+                <div className={style.stat}>
+                    <span>Altitude</span>
+                    <span>8848m</span>
+                </div>
+                <div className={style.stat}>
+                    <span>Tracks</span>
+                    <span>20</span>
+                </div>
+                <div className={style.stat}>
+                    <span>Tourists/year</span>
+                    <span>40000+</span>
+                </div>
+                </div>
             </div>
         </div>
         
