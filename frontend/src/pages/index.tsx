1bfe5ddebff46792501d9257fe7469a8833db148
lokendra <lokendrachaulagain803@gmail.com>
2022-08-24 16:26:07 +0545

    ready for merge

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import CardCarousel from "../components/CardCarousel";
 import Footer from "../components/Footer";
-import HeroCarousel from "../components/HeroCarousel";
 import Navbar from "../components/Navbar";
 import NewsLetter from "../components/NewsLetter";
 
