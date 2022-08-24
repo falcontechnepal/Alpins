4a8cdab8dc1fad42086b3ec25ec15db5d1ddc030
sagarkhadka <khadkasagar109@gmail.com>
2022-08-24 16:43:27 +0545

    contact done

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -7,21 +7,22 @@ const Hero = () => {
         <div className={`${style.heroBody} container`}>
             <div className="row">
                 <div className={`${style.leftBody} col-md-6`}>
-                    <h1>Amazing Mountan to explore</h1>
+                    {/* <h1>Amazing Mountan to explore</h1> */}
+                    <h1 className={`${style.heading} h1`}>Amazing Mountain to explore</h1>
                     <p>Do not follow where the path may lead. Go instead where there is no path and leave a trail.</p>
                 </div>
                 <div className={`${style.rightBody} col-md-4`}>
                 <div className={style.stat}>
-                    <span>Altitude</span>
-                    <span>8848m</span>
+                    <span className='h6'>Altitude</span>
+                    <span className='h4'>8848m</span>
                 </div>
                 <div className={style.stat}>
-                    <span>Tracks</span>
-                    <span>20</span>
+                    <span className='h6'>Tracks</span>
+                    <span className='h4'>20</span>
                 </div>
                 <div className={style.stat}>
-                    <span>Tourists/year</span>
-                    <span>40000+</span>
+                    <span className='h6'>Tourists/year</span>
+                    <span className='h4'>40000+</span>
                 </div>
                 </div>
             </div>
---
+++ frontend/src/pages/contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Contactform from '../components/Contactform'
+import Maps from '../components/Maps'
+import Navbar from '../components/Navbar'
+
+const contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <Maps />
+      <Contactform />
+    </div>
+  )
+}
+
+export default contact
